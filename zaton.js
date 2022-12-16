359c94aa966178988675ce7ff33b89aaf6fe7ff8
Nikola <nikolaristic012@gmail.com>
2022-12-16 07:55:13 +0100

    added java script

--- zaton.js
+++ zaton.js
@@ -1,3 +1,20 @@
-function greeting() => {
-    console.log('Dobrodosli');
-}
+
+
+let inbox = 100;
+let user ='Ed';
+console.log(inbox)
+
+alert("dobrodosli");
+
+// hello Zaton
+
+console.log(inbox/5)
+
+
+const bank1 =100;
+const bank1 =50;
+
+const allBanks= bank1 + bank2;
+
+const greeting="Welcome to our website";
+const user='Ed';
